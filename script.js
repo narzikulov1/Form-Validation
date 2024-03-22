2193c2a9d998b4aa1464ccb0885c3df4fb20f301
Sardor Narzikulov <sardornarzikulovv2002@gmail.com>
2024-03-22 15:17:38 +0500

    make phone number validation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let phoneError = document.getElementById("phone-error");
+
 let emailError = document.getElementById("email-error");
 let messageError = document.getElementById("message-error");
 let submitError = document.getElementById("submit-error");
@@ -6,20 +6,43 @@ let submitError = document.getElementById("submit-error");
 // validation name
 let nameError = document.getElementById("name-error");
 
-function validateName(){
+function validateName() {
     let name = document.getElementById("full-name").value;
 
-    if(name.length == 0){
+    if (name.length == 0) {
         nameError.innerHTML = "Name is required";
         return false;
     }
-    
-    
-    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+
+
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
         nameError.innerHTML = "Write full name";
         return false;
     }
 
     nameError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`
+    return true;
+}
+
+// validation phone
+let phoneError = document.getElementById("phone-error");
+
+function validatePhone() {
+    let phone = document.getElementById("phone-number").value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = "Phone no is required";
+        return false;
+    }
+    if(phone.length !== 9){
+        phoneError.innerHTML = "Phone no should be 9 digits";
+        return false;
+    }
+    if(!phone.match(/^[0-9]{9}$/)){
+        phoneError.innerHTML = "Only digits please";
+        return false;
+    }
+
+    phoneError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`
     return true;
 }
